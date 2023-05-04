ecb6ee2c2521b2c89f7f8a8862f1d34b09ebff82
Sungha-chu <58934772+Sungha-chu@users.noreply.github.com>
2023-05-04 11:43:38 +0800

    Add files via upload

--- assets/js/bluetooth.js
+++ assets/js/bluetooth.js
@@ -1,31 +1,11 @@
-function init_bt(){
-    // 请求访问蓝牙设备
-    navigator.bluetooth.requestDevice({ 
-        filters: [{ services: ['serial_port'] }] 
-    })
-    .then(device => {
-        // 连接到蓝牙设备
-        return device.gatt.connect(); 
-    })
-    .then(server => {
-        // 获取服务
-        return server.getPrimaryService('serial_port'); 
-    })
-    .then(service => {
-        // 获取特征
-        return service.getCharacteristic('rx'); 
-    })
-    .then(characteristic => {
-        // 监听数据变化
-        characteristic.addEventListener('characteristicvaluechanged', event => {
-        const value = new TextDecoder().decode(event.target.value);
-        // 显示接收到的数据
-        console.log(value);
-        });
-        // 启动通知
-        return characteristic.startNotifications(); 
-    })
-    .catch(error => {
-        console.error(error);
-    });
+async function init_bt(){
+    try {
+        const baudRate = 115200;
+        const port = await navigator.serial.requestPort();
+        await port.open({ baudRate });
+        console.log('Selected serial port:'+port.name);
+        // 在這裡進行串口的讀寫等操作
+    } catch (error) {
+        console.error('Error opening serial port:'+ error);
+    }
 }
